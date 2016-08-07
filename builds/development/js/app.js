5fc718750bff2892b8476850efb9584423f6b7d6
Chuck McQuilkin <ctmcquilkin@gmail.com>
2016-08-06 19:25:44 -0500

    refactored app to add edit and add new screens using routeParams.

--- builds/development/js/app.js
+++ builds/development/js/app.js
@@ -1,9 +1,10 @@
-var animateApp = angular.module('animateApp', ['ngRoute', 'ngAnimate', 'firebase'])
-.constant('FIREBASE_ROOT_URL', 'https://eat-right.firebaseio.com/favoritelist/');
+angular.module('eatRight', ['ngRoute', 'ngAnimate', 'firebase'])
+
+.value('fbURL', 'https://eat-right.firebaseio.com/favoritelist/food-items/')
 
 /*
 URL Scheme will be three lists, on for activities, 
-one for food and one for totals in the followin format:
+one for food and one for totals in the following format:
 
 // https://eat-right.firebaseio.com/favoritelist/
 {
@@ -27,185 +28,66 @@ one for food and one for totals in the followin format:
   }
 }
 
-*/
-
-/*
-
-To convert minutes to calories:
+To convert minutes of exercise to calories:
 
 Men:
-
 Calories Burned = [(Age x 0.2017) — (Weight x 0.09036) + (Heart Rate x 0.6309) — 55.0969] x Time / 4.184.
 
 Women:
-
 Calories Burned = [(Age x 0.074) — (Weight x 0.05741) + (Heart Rate x 0.4472) — 20.4022] x Time / 4.184.
 
 */
 
-animateApp.config(function($routeProvider) {
-    $routeProvider
-    	.when('/', {
-    		templateUrl: 'views/page-home.html',
-            controller: 'mainController'
-    	})
-    	.when('/activity', {
-    		templateUrl: 'views/page-activity.html',
-            controller: 'activityController'
-    	})
-        .when('/add-activity', {
-            templateUrl: 'views/page-add-activity.html',
-            controller: 'addActivityController'
-        })
-        .when('/dashboard', {
-            templateUrl: 'views/page-dashboard.html',
-            controller: 'dashboardController'
-        })
-    	.when('/food', {
-    		templateUrl: 'views/page-food.html',
-            controller: 'favoriteFoodCtrl'
-    	})
-        .when('/add-food', {
-            templateUrl: 'views/page-add-food.html',
-            controller: 'addFoodController'
-        });
+.factory('Foods', function($firebase, fbURL) {
+  return $firebase(new Firebase(fbURL));
+})
 
-});
+.config(function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      controller:'ListCtrl',
+      templateUrl:'views/list.html'
+    })
+    .when('/edit/:foodId', {
+      controller:'EditCtrl',
+      templateUrl:'views/detail.html'
+    })
+    .when('/new', {
+      controller:'CreateCtrl',
+      templateUrl:'views/detail.html'
+    })
+    .otherwise({
+      redirectTo:'/'
+    });
+})
 
-animateApp.controller('mainController', function($scope) {
-    $scope.pageClass = 'page-home';
-});
+.controller('ListCtrl', function($scope, Foods) {
+  $scope.pageClass = 'page-food';
+  $scope.foods = Foods;
+})
 
-animateApp.controller('activityController', function($scope, $firebaseArray, FIREBASE_ROOT_URL) {
+.controller('CreateCtrl', function($scope, $location, $timeout, Foods) {
+  $scope.pageClass = 'page-add-food';
+  $scope.save = function() {
+    Foods.$add($scope.food, function() {
+      $timeout(function() { $location.path('/'); });
+    });
+  };
+})
+
+.controller('EditCtrl',
+  function($scope, $location, $routeParams, $firebase, fbURL) {
     $scope.pageClass = 'page-activity';
-     $scope.orderList = "name";
-     $scope.query;
-    $scope.activities = [
-      {name: "Walking", category: "aerobic", calories: 4.78},
-      {name: "Running", category: "aerobic", calories: 1},
-      {name: "Bicycling", category: "aerobic", calories: 14},
-      {name: "Swimming", category: "aerobic", calories: 8},
-    ];
-      var list = $scope.activities;    
-      var arrayLength = list.length;
+    var foodUrl = fbURL + $routeParams.foodId;
+    $scope.food = $firebase(new Firebase(foodUrl));
 
-      // Sync to Firebase as Array
-      this.activityList = $firebaseArray(new Firebase(FIREBASE_ROOT_URL + 'activity-items'));
+    $scope.destroy = function() {
+      $scope.food.$remove();
+      $location.path('/');
+    };
 
-      // Add Activity
-      this.addActivity = function () {
-
-        this.activityList.$add({
-          name: this.name,
-          date: new Date().valueOf()
-        });
-
-        this.name = null;
-      }.bind(this);
-
-      // Remove Activity
-      this.removeItem = function (item) {
-        this.activityList.$remove(item);
-      };
-
-     // Add New Activity Button
-      $scope.addActivity = function() {
-        if($scope.query !== ''){
-            $scope.newCategory = $scope.newCategory.toLowerCase();
-            // Need to add method to convert minutes to calories
-            $scope.activities.push({name: $scope.newName, category: $scope.newCategory, calories: $scope.newMinutes});
-            // getImage($scope, $http, arrayLength); // To Do: need to add icon for each category
-           $scope.newName = '';
-           $scope.newCategory = '';
-           $scope.newMinutes = '';
-      
-        }
-      };
-
-});
-
-animateApp.controller('addActivityController', function($scope) {
-    $scope.pageClass = 'page-add-activity';
-});
-
-animateApp.controller('favoriteFoodCtrl', function($scope, $firebaseArray, FIREBASE_ROOT_URL) {
-    $scope.pageClass = 'page-food';
-     $scope.orderList = "name";
-     $scope.query;
-     $scope.foods = [
-      {name: "Apple", category: "fruit", calories: 95},
-      {name: "Coffee", category: "drink", calories: 1},
-      {name: "Milk (nonfat)", category: "drink", calories: 83},
-      {name: "Cereal", category: "grain", calories: 210},
-      {name: "Salad (endless summer)", category: "vegetable", calories: 150},
-      {name: "Salad (spinach)", category: "vegetable", calories: 140},
-     ];
-      var list = $scope.foods;
-      var arrayLength = list.length;
-
-      // Sync to Firebase as Array
-      this.foodList = $firebaseArray(new Firebase(FIREBASE_ROOT_URL + 'food-items'));
-
-      // Add Food
-      this.addFood = function () {
-
-        this.foodList.$add({
-          name: this.name,
-          date: new Date().valueOf()
-        });
-
-        this.name = null;
-      }.bind(this);
-
-      // Remove Food
-      this.removeItem = function (item) {
-        this.foodList.$remove(item);
-      };
-
-      
-         // Add New Food Button
-      $scope.addFavoriteFood = function() {
-        if($scope.query !== ''){
-            $scope.newCategory = $scope.newCategory.toLowerCase();
-            $scope.foods.push({name: $scope.newName, category: $scope.newCategory, calories: $scope.newCalories});
-            // getImage($scope, $http, arrayLength); // o Do: need to add icon for each category
-             $scope.newName = '';
-             $scope.newCategory = '';
-             $scope.newCalories = '';
-           console.log($scope.newCalories);
-        }
-      };
-});
-
-animateApp.controller('addFoodController', function($scope) {
-    $scope.pageClass = 'page-add-food';
-});
-
-animateApp.controller('dashboardController', function($scope) {
-    $scope.pageClass = 'page-dashboard';
-    $scope.today = new Date();
-       $scope.orderList = "name";
-       $scope.query;
-      $scope.activities = [
-        {name: "340 Burned", category: "aerobic", calories: 4.78},
-        {name: "545 Carbs", category: "aerobic", calories: 14},
-        {name: "589 Protien", category: "aerobic", calories: 8},
-        {name: "955 Fat", category: "aerobic", calories: 8},
-      ];
-        var list = $scope.activities;    
-        var arrayLength = list.length;
-        
-       // Add New Activity Button
-        $scope.addActivity = function() {
-          if($scope.query !== ''){
-              $scope.newCategory = $scope.newCategory.toLowerCase();
-              // Need to add method to convert minutes to calories
-              $scope.activities.push({name: $scope.newName, category: $scope.newCategory, calories: $scope.newMinutes});
-              // getImage($scope, $http, arrayLength); // To Do: need to add icon for each category
-             $scope.newName = '';
-             $scope.newCategory = '';
-             $scope.newMinutes = '';
-        
-          }
-        };
+    $scope.save = function() {
+      $scope.food.$save();
+      $location.path('/');
+    };
 });
